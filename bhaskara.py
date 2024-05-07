24f451bf3597c724911fa0bb17385d5dee75acbc
aremartins <arealbmartinsan@gmail.com>
2024-05-07 11:55:15 -0300

    fix: remove repetição de código

--- bhaskara.py
+++ bhaskara.py
@@ -10,15 +10,14 @@ c = float(input("Informe o valor para c: "))
 delta = b**2 - 4*a*c
 
 
-
-if(delta == 0):
-    raiz1 = -b + math.sqrt(delta) / (2 * a)        
+if(delta >= 0 ):
+    raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+    raiz2 = (-b - math.sqrt(delta)) / ( 2 * a)  
+if(delta == 0):       
     print("A raiz quadrada é {}".format(raiz1))
 elif delta < 0:
     print("Esta equação não possui raízes reais")
-else:
-    raiz1 = (-b + math.sqrt(delta)) / (2 * a)
-    raiz2 = (-b - math.sqrt(delta)) / ( 2 * a)    
+else:    
     print("As raízes da equação são {}, {}".format(raiz1, raiz2))  
    
 
