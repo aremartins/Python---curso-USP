dd9c73f830f4d508dc910220c0bde4f9fdd977e4
aremartins <arealbmartinsan@gmail.com>
2024-05-24 07:20:45 -0300

    fix: correção exercicio numeros primos

--- priminalidade.py
+++ priminalidade.py
@@ -5,7 +5,7 @@ contador = 0
 for i in range(1, n + 1):
      if n % i == 0:
          contador += 1
-if(contador == 2):
-    print("primo")
+if(contador == 2):    
+    print("primo", i)    
 else:
     print("não primo")   
